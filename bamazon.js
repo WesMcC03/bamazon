531cc8fbc06931fa10b7878ea39f988f8a3ba1a2
Wes McC <wkmccowin@yahoo.com>
2019-05-17 11:17:37 -0700

    updated logic with inquirer and connected database to server. Still getting undeifined for objects, will work on that error and also inquirer functions.

--- bamazon.js
+++ bamazon.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const inquirer =require('inquirer')
+const inquirer = require('inquirer')
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -12,44 +12,86 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    start();
   });
   
-  function runSearch() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "Welcome to Bamazon! How may we be of service today?",
-        choices: [
-          "View Products for sale",
-          "View Low Inventory",
-          "Add to Inventory",
-          "Add New Product",
-          "exit"
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.action) {
-        case "View Products for sale":
-          ProductView();
-          break;
-  
-        case "View Low Inventory":
-          LowProductView();
-          break;
-  
-        case "Add to Inventory":
-          AddInventory();
-          break;
-  
-        case "Add New Product":
-          AddProduct();
-          break;
-            
-        case "exit":
-          connection.end();
-          break;
-        }
-      })
-    }
+
+  function start() {
+    var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE ?";
+    connection.query(query, function(err, res) {
+        // if (err) throw err;
+        for (var i=0; i < query.length; i++) {
+            console.log("item_id: " +query[i].item_id+"\nproduct_name :" +query[i].product_name+"\ndepartment_name: "
+        +query[i].department_name+"\nprice: " +query[i].price+ "\nstock_quantity: "+query[i].stock_quantity+"\n-----------------------\n"
+        )};
+        inquirer
+        .prompt([
+          {
+            name: "product_id",
+            type: "input",
+
+            message: "What is the product ID number of the item yoou wish to purchase??"
+          },
+          {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?"
+          }
+        ])
+
+     }
+    )};
+
+    // start();
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        inquirer
+        .prompt([
+          {
+            name: "choice",
+            type: "rawlist",
+            choices: function() {
+              var productArray = [];
+              for (var i = 0; i < results.length; i++) {
+                choiceArray.push(results[i].product_id);
+              }
+              return productArray;
+            },
+            message: "What is the product ID number of the item yoou wish to purchase??"
+          },
+          {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?"
+          }
+        ])
+        .then(function(answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+            "INSERT INTO auctions SET ?",
+            {
+                item_name: answer.item,
+                category: answer.category,
+                starting_bid: answer.startingBid || 0,
+                highest_bid: answer.startingBid || 0
+            },
+            function(err) {
+                if (err) throw err;
+                console.log("Your auction was created successfully!");
+                // re-prompt the user for if they want to bid or post
+                start();
+            }
+            );
+        
+    });
