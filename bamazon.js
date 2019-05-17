9007f131776325d01202272998de6c34a1174d18
Wes McC <wkmccowin@yahoo.com>
2019-05-17 14:19:08 -0700

    able to get product list to show on the command line, still need to get inquirer to find data from table mysql successfulyy through inquirer.

--- bamazon.js
+++ bamazon.js
@@ -17,30 +17,36 @@ connection.connect(function(err) {
   
 
   function start() {
-    var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE ?";
+    var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products";
     connection.query(query, function(err, res) {
         // if (err) throw err;
-        for (var i=0; i < query.length; i++) {
-            console.log("item_id: " +query[i].item_id+"\nproduct_name :" +query[i].product_name+"\ndepartment_name: "
-        +query[i].department_name+"\nprice: " +query[i].price+ "\nstock_quantity: "+query[i].stock_quantity+"\n-----------------------\n"
+        for (var i=0; i < res.length; i++) {
+            console.log("item_id: " +res[i].item_id+"\nproduct_name :" +res[i].product_name+"\ndepartment_name: "
+        +res[i].department_name+"\nprice: " +res[i].price+ "\nstock_quantity: "+res[i].stock_quantity+"\n-----------------------\n"
         )};
-        inquirer
-        .prompt([
-          {
+        inquirer.prompt({
             name: "product_id",
             type: "input",
+            message: "What is the product ID number of the item yoou wish to purchase??" ,
+            }).then(function(answer){
+                var query = "SELECT item_id FROM products";
+                connection.query(query, {product_id:answer.product_id}, function(err,res){
+            });
+        })
+    })
+};
+            
+          
+   
+   
+   
+            //     name: "quantity",
+        //     type: "input",
+        //     message: "How many would you like to purchase?"
+        //   }
+        // ])
 
-            message: "What is the product ID number of the item yoou wish to purchase??"
-          },
-          {
-            name: "quantity",
-            type: "input",
-            message: "How many would you like to purchase?"
-          }
-        ])
-
-     }
-    )};
+ 
 
     // start();
 
